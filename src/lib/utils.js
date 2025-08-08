c909f2b1e774ff03c4cab3817ad4c0eb83fb896d
Valery Shutikhin <shutikhinv@gmail.com>
2025-08-08 05:08:54 +0300

    sonaaaar

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,6 +2,7 @@
 const getRandomInt = (min = 0, max = 50) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
+// NOSONAR: Math.random usage is acceptable here - game logic doesn't require cryptographic security
 const getRandomSym = () => {
   const symbols = ['+', '-', '*']
   return symbols[Math.floor(Math.random() * symbols.length)]
