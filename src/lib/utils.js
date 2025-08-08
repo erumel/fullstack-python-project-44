6dc78687be87393196b2f086a0d5498b7d1c92bd
Valery Shutikhin <shutikhinv@gmail.com>
2025-08-08 05:04:52 +0300

    sonar

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,10 +2,9 @@
 const getRandomInt = (min = 0, max = 50) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
-// NOSONAR: Math.random usage is safe in this context (game logic)
 const getRandomSym = () => {
   const symbols = ['+', '-', '*']
-  return symbols[Math.floor(Math.random() * 3)]
+  return symbols[Math.floor(Math.random() * symbols.length)]
 }
 
 export default getRandomInt
