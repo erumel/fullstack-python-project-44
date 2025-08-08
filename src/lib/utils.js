e03a1f6fba7b2cac61281e6599bcf349f8f94458
Valery Shutikhin <shutikhinv@gmail.com>
2025-08-08 06:15:06 +0300

    sonar

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,10 @@
+// nosonar: This is game logic, not security-sensitive code
 const getRandomInt = (min = 0, max = 50) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
-// nosonar: This is game logic, not security-sensitive code
 const getRandomSym = () => {
   const symbols = ['+', '-', '*']
-  return symbols[Math.floor(Math.random() * symbols.length)]
+  return symbols[Math.floor(Math.random() * symbols.length)] // nosonar: This is game logic, not security-sensitive code
 }
 
 export default getRandomInt
