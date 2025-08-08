8faea170f9435a8e4104af146a6f8120b99a9087
Valery Shutikhin <shutikhinv@gmail.com>
2025-08-08 03:12:09 +0300

    Update comment

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
-// nosemgrep: javascript.lang.security.audit.math-random-used.math-random-used
+// NOSONAR: Math.random usage is safe in this context (game logic)
 const getRandomInt = (min = 0, max = 50) =>
   Math.floor(Math.random() * (max - min + 1)) + min
 
